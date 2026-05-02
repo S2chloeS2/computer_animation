3381d171dfeed7f07c2778f48afad38e652d3935
S2chloeS2 <cl4490@columbia.edu>
2026-05-02 15:15:09 -0400

    Compelte step4; extrapolation,PIC, Flip, Blending

--- nemo-pa6-release/src/nemo/solvers/fluid_flip_solver.py
+++ nemo-pa6-release/src/nemo/solvers/fluid_flip_solver.py
@@ -283,21 +283,55 @@ class FluidFlipSolver(SolverBase):
     def _transfer_grid_velocity_to_particles(self, par_voxel_coord: nparray, state_in: State, state_out: State):
         model = self.model
 
-        # Here comes the extrapolation that I talked about in the lecture.
-        # This extrapolation is to avoid using undefined velocities for grid-to-particle interpolation.
-        # Again, following is the code that I used in my implementation as a reference.
-        #
-        # u_x_now = extrapolate_velocity_field(state_out.fluid_u_x, self._valid_u_x)
-        # u_y_now = extrapolate_velocity_field(state_out.fluid_u_y, self._valid_u_y)
-        # u_x_old = extrapolate_velocity_field(state_in.fluid_u_x, self._valid_u_x)
-        # u_y_old = extrapolate_velocity_field(state_in.fluid_u_y, self._valid_u_y)
+        dx = model.fluid_cell_size
 
-        # TODO: Now transfer the extrapolated grid velocity to particles
+        # extrapolate grid velocities into air voxels to avoid zero-velocity contamination
+        u_x_now = extrapolate_velocity_field(state_out.fluid_u_x, self._valid_u_x)
+        u_y_now = extrapolate_velocity_field(state_out.fluid_u_y, self._valid_u_y)
+        u_x_old = extrapolate_velocity_field(state_in.fluid_u_x, self._valid_u_x)
+        u_y_old = extrapolate_velocity_field(state_in.fluid_u_y, self._valid_u_y)
 
-        # As a hint, the final particle velocity is a weighted blend of the PIC and FLIP velocities.
-        #
-        # state_out.fluid_particle_qd[i, 0] = u_x_pic * (1.0 - self._FLIP_FACTOR) + u_x_flip * self._FLIP_FACTOR
-        # state_out.fluid_particle_qd[i, 1] = u_y_pic * (1.0 - self._FLIP_FACTOR) + u_y_flip * self._FLIP_FACTOR
+        n_particles = state_out.fluid_particle_q.shape[0]
+        for p in range(n_particles):
+            px, py = state_out.fluid_particle_q[p, 0], state_out.fluid_particle_q[p, 1]
+            wx, wy = px / dx, py / dx
+
+            # --- interpolate u_x at (wx, wy-0.5) ---
+            fx, fy = wx, wy - 0.5
+            i0, j0 = int(np.floor(fx)), int(np.floor(fy))
+            i1, j1 = i0 + 1, j0 + 1
+            s, t = fx - i0, fy - j0
+            res_y_x = u_x_now.shape[0]
+            res_x_x = u_x_now.shape[1]
+
+            def sample(grid, jj, ii):
+                jj = max(0, min(jj, grid.shape[0] - 1))
+                ii = max(0, min(ii, grid.shape[1] - 1))
+                return grid[jj, ii]
+
+            u_x_pic  = ((1-s)*(1-t)*sample(u_x_now, j0, i0) + s*(1-t)*sample(u_x_now, j0, i1)
+                      + (1-s)*t    *sample(u_x_now, j1, i0) + s*t    *sample(u_x_now, j1, i1))
+            u_x_old_i = ((1-s)*(1-t)*sample(u_x_old, j0, i0) + s*(1-t)*sample(u_x_old, j0, i1)
+                       + (1-s)*t    *sample(u_x_old, j1, i0) + s*t    *sample(u_x_old, j1, i1))
+
+            # --- interpolate u_y at (wx-0.5, wy) ---
+            fx, fy = wx - 0.5, wy
+            i0, j0 = int(np.floor(fx)), int(np.floor(fy))
+            i1, j1 = i0 + 1, j0 + 1
+            s, t = fx - i0, fy - j0
+
+            u_y_pic  = ((1-s)*(1-t)*sample(u_y_now, j0, i0) + s*(1-t)*sample(u_y_now, j0, i1)
+                      + (1-s)*t    *sample(u_y_now, j1, i0) + s*t    *sample(u_y_now, j1, i1))
+            u_y_old_i = ((1-s)*(1-t)*sample(u_y_old, j0, i0) + s*(1-t)*sample(u_y_old, j0, i1)
+                       + (1-s)*t    *sample(u_y_old, j1, i0) + s*t    *sample(u_y_old, j1, i1))
+
+            # FLIP: particle old velocity + grid velocity change
+            u_x_flip = state_in.fluid_particle_qd[p, 0] + (u_x_pic - u_x_old_i)
+            u_y_flip = state_in.fluid_particle_qd[p, 1] + (u_y_pic - u_y_old_i)
+
+            # PIC/FLIP blend
+            state_out.fluid_particle_qd[p, 0] = u_x_pic * (1.0 - self._FLIP_FACTOR) + u_x_flip * self._FLIP_FACTOR
+            state_out.fluid_particle_qd[p, 1] = u_y_pic * (1.0 - self._FLIP_FACTOR) + u_y_flip * self._FLIP_FACTOR
 
     def step(self, state_in: State, state_out: State, dt: float | None = None):
         """Step the solver forward in time."""
